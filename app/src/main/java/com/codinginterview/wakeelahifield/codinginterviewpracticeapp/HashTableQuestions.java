17bf8ddb1b3cbfd038b8891b71629ee14089af68
Devin_Wakefield <devin.wakefield@gmail.com>
2015-06-02 08:17:17 -0700

    made barebones setup of activities, and buttons to link to each activity. Now we can modify them with the power and the force! fun!

--- app/src/main/java/com/codinginterview/wakeelahifield/codinginterviewpracticeapp/HashTableQuestions.java
+++ app/src/main/java/com/codinginterview/wakeelahifield/codinginterviewpracticeapp/HashTableQuestions.java
@@ -1,26 +1,23 @@
 package com.codinginterview.wakeelahifield.codinginterviewpracticeapp;
 
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 
-public class MainActivity extends ActionBarActivity {
+public class HashTableQuestions extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_hash_table_questions);
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_hash_table_questions, menu);
         return true;
     }
 
@@ -37,11 +34,5 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    public void to_topics(View view){
-        Intent intent = new Intent(this, Topics.class);
-        startActivity(intent);
-
     }
 }
--- app/src/main/java/com/codinginterview/wakeelahifield/codinginterviewpracticeapp/Topics.java
+++ app/src/main/java/com/codinginterview/wakeelahifield/codinginterviewpracticeapp/Topics.java
@@ -8,19 +8,18 @@ import android.view.MenuItem;
 import android.view.View;
 
 
-public class MainActivity extends ActionBarActivity {
+public class Topics extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_topics);
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_topics, menu);
         return true;
     }
 
@@ -39,9 +38,8 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void to_topics(View view){
-        Intent intent = new Intent(this, Topics.class);
+    public void to_hashTables(View view){
+        Intent intent = new Intent(this, HashTableQuestions.class);
         startActivity(intent);
-
     }
 }
