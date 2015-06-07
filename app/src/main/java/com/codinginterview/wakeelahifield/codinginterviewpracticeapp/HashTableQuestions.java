2f7702208fb8f1821509aa34181b7fcddd0a6f64
Devin_Wakefield <devin.wakefield@gmail.com>
2015-06-07 11:51:57 -0700

    Set up basic functionality to read a question from the json file and present it to the user.
    Played with Question activity (right now the HashTableQuestions.java) UI and general functionality.

--- app/src/main/java/com/codinginterview/wakeelahifield/codinginterviewpracticeapp/HashTableQuestions.java
+++ app/src/main/java/com/codinginterview/wakeelahifield/codinginterviewpracticeapp/HashTableQuestions.java
@@ -4,6 +4,15 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.RadioButton;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 
 public class HashTableQuestions extends ActionBarActivity {
@@ -12,6 +21,12 @@ public class HashTableQuestions extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hash_table_questions);
+
+        try {
+            testJson();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -35,4 +50,68 @@ public class HashTableQuestions extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    //TODO: make it so that it can switch from one question to the next after being answered.
+    //TODO: keep track of which questions have been answered that works after app has been closed and turned off (write to file!)
+    //TODO: come up with a way to find which question to ask next. random? sequential?
+
+
+    public void testJson() throws JSONException {
+        TextView testView = (TextView) findViewById(R.id.Question);
+        String json = loadJSONFromAsset();
+        //testView.setText(json);
+
+        //TODO: is there a better way to do this programmatically?
+
+        RadioButton a1 = (RadioButton) findViewById(R.id.a1);
+        RadioButton a2 = (RadioButton) findViewById(R.id.a2);
+        RadioButton a3 = (RadioButton) findViewById(R.id.a3);
+        RadioButton a4 = (RadioButton) findViewById(R.id.a4);
+        RadioButton a5 = (RadioButton) findViewById(R.id.a5);
+
+        JSONObject obj = new JSONObject(json);
+
+
+
+        JSONArray hash_table_Qs = obj.getJSONArray("Hash Tables");
+        String firstQuestion = hash_table_Qs.getJSONObject(0).getString("question");
+        testView.setText(firstQuestion);
+
+        JSONArray q1_answers = hash_table_Qs.getJSONObject(0).getJSONArray("choices");
+
+
+        //TODO: again, can I do this automatically?
+        a1.setText(q1_answers.getString(0));
+        a2.setText(q1_answers.getString(1));
+        a3.setText(q1_answers.getString(2));
+        a4.setText(q1_answers.getString(3));
+        a5.setText(q1_answers.getString(4));
+
+    }
+
+    public String loadJSONFromAsset() {
+        String json;
+        try {
+
+            InputStream is = getAssets().open("questions.json");
+
+            int size = is.available();
+
+            byte[] buffer = new byte[size];
+
+            is.read(buffer);
+
+            is.close();
+
+            json = new String(buffer, "UTF-8");
+
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+        return json;
+
+    }
+
 }
